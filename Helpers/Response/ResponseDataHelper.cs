77d0728b792f834211983905af835f1233003fb3
Daniela Pereira Pochini (TECH - Terceiro) <daniela.pochini@bs2tecnologia.com.br>
2021-03-13 17:13:31 -0300

    Frameworks Metodo Get

---
+++ Helpers/Response/ResponseDataHelper.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace WebServiceAutomation.Helpers.Response
+{
+    public class ResponseDataHelper
+    {
+         
+        public static T DeserializeJsonResponse<T>(string responseData) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(responseData);
+        }
+
+        public static T DeserializeXmlResponse<T>(string responseData) where T : class
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            TextReader textReader = new StringReader(responseData);
+            return (T)xmlSerializer.Deserialize(textReader);
+        }
+    }
+}

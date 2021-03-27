11f1b86bb9d9c2ad4bff95e334354cad1e255ce3
Daniela Pereira Pochini (TECH - Terceiro) <daniela.pochini@bs2tecnologia.com.br>
2021-03-27 20:45:51 -0300

    Criação método DELETE

---
+++ DeleteEndPoint/TestDeleteEndPoint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceAutomation.Helpers.Request;
+using WebServiceAutomation.Helpers.Response;
+using WebServiceAutomation.Model;
+using WebServiceAutomation.Model.XmlModel;
+using Xunit;
+
+namespace WebServiceAutomation.DeleteEndPoint
+{
+    public class TestDeleteEndPoint
+    {
+        private string postUrl = "http://localhost:8080/laptop-bag/webapi/api/add/"; 
+        private string deleteUrl = "http://localhost:8080/laptop-bag/webapi/api/delete/";  
+        private Random random = new Random();
+
+        [Fact]
+        public void TestDelete()
+        {
+           int id = random.Next(1000);
+           RequestHelper.AddRecord(postUrl, id);
+
+            using(HttpClient httpClient = new HttpClient())
+            {
+                Task<HttpResponseMessage> httpResponseMessage = httpClient.DeleteAsync(deleteUrl + id);
+                HttpStatusCode httpStatusCode = httpResponseMessage.Result.StatusCode;
+                Assert.Equal(200, (int)httpStatusCode);
+
+                httpResponseMessage = httpClient.DeleteAsync(deleteUrl + id);
+                httpStatusCode = httpResponseMessage.Result.StatusCode;
+                Assert.Equal(404, (int)httpStatusCode);
+            }
+        }
+         
+       
+    }
+}
---
+++ Helpers/Request/RequestHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebServiceAutomation.Model;
+using Xunit;
+
+namespace WebServiceAutomation.Helpers.Request
+{
+    public class RequestHelper
+    {  
+        public static void AddRecord(string postUrl, int id)
+        {
+            string xmlMediaType = "application/xml";
+
+            string xmlData =
+                                           "<Laptop>" +
+                                              "<BrandName>Alienware</BrandName>" +
+                                                "<Features>" +
+                                                   "<Feature>8th Generation Intel® Core™ i5 - 8300H</Feature>" +
+                                                   "<Feature>Windows 10 Home 64 - bit English</Feature>" +
+                                                   "<Feature>NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6</Feature>" +
+                                                   "<Feature>8GB, 2x4GB, DDR4, 2666MHz</Feature>" +
+                                                 "</Features>" +
+                                              "<Id>" + id + "</Id>" +
+                                              "<LaptopName>Alienware M17</LaptopName>" +
+                                           "</Laptop>";
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+            {"Accept","application/xml"}
+            };
+
+            var restResponse = HttpClientHelper.PerformPostRequest(postUrl, xmlData, xmlMediaType, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+        }
+    }
+}

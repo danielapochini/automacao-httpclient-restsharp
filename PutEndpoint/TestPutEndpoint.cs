56bb2d236e460d1b948588764f42e20c2dd32b8b
Daniela Pereira Pochini (TECH - Terceiro) <daniela.pochini@bs2tecnologia.com.br>
2021-03-27 20:13:46 -0300

    Usando Put Framework nas classes de Test

--- PutEndpoint/TestPutEndpoint.cs
+++ PutEndpoint/TestPutEndpoint.cs
@@ -143,5 +143,109 @@ namespace WebServiceAutomation.PutEndpoint
 
             Assert.Contains("1 TB of SSD", jsonObject.Features.Feature);
         }
+
+        [Fact]
+        public void TestPutWithJsonAndHelperClass()
+        {
+            int id = random.Next(1000);
+
+            string jsonData = "{" +
+                        "\"BrandName\": \"Alienware\"," +
+                        "\"Features\": {" +
+                        "\"Feature\": [" +
+                        "\"8th Generation Intel® Core™ i5-8300H\"," +
+                        "\"Windows 10 Home 64-bit English\"," +
+                        "\"NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6\"," +
+                        "\"8GB, 2x4GB, DDR4, 2666MHz\"" +
+                        "]" +
+                        "}," +
+                        "\"Id\": " + id + "," +
+                        "\"LaptopName\": \"Alienware M17\"" +
+                    "}";
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+            {"Accept","application/json"}
+            };
+
+            restResponse = HttpClientHelper.PerformPostRequest(postUrl, jsonData, jsonMediaType, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+
+            jsonData = "{" +
+                        "\"BrandName\": \"Alienware\"," +
+                        "\"Features\": {" +
+                        "\"Feature\": [" +
+                        "\"8th Generation Intel® Core™ i5-8300H\"," +
+                        "\"Windows 10 Home 64-bit English\"," +
+                        "\"NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6\"," +
+                        "\"1 TB of SSD\"," +
+                        "\"8GB, 2x4GB, DDR4, 2666MHz\"" +
+                        "]" +
+                        "}," +
+                        "\"Id\": " + id + "," +
+                        "\"LaptopName\": \"Alienware M17\"" +
+                    "}";
+
+            restResponse = HttpClientHelper.PerformPutRequest(putUrl, jsonData, jsonMediaType, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+
+            restResponse = HttpClientHelper.PerformGetRequest(getUrl + id, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+
+            JsonRootObject jsonObject = ResponseDataHelper.DeserializeJsonResponse<JsonRootObject>
+                (restResponse.responseContent);
+
+            Assert.Contains("1 TB of SSD", jsonObject.Features.Feature);
+        }
+
+
+        [Fact]
+        public void TestPutWithXmlAndHelperClass()
+        {
+            int id = random.Next(1000);
+            string xmlData =
+                                           "<Laptop>" +
+                                              "<BrandName>Alienware</BrandName>" +
+                                                "<Features>" +
+                                                   "<Feature>8th Generation Intel® Core™ i5 - 8300H</Feature>" +
+                                                   "<Feature>Windows 10 Home 64 - bit English</Feature>" +
+                                                   "<Feature>NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6</Feature>" +
+                                                   "<Feature>8GB, 2x4GB, DDR4, 2666MHz</Feature>" +
+                                                 "</Features>" +
+                                              "<Id>" + id + "</Id>" +
+                                              "<LaptopName>Alienware M17</LaptopName>" +
+                                           "</Laptop>";
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+            {"Accept","application/xml"}
+            };
+
+            restResponse = HttpClientHelper.PerformPostRequest(postUrl, xmlData, xmlMediaType, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+
+            xmlData =
+                               "<Laptop>" +
+                                  "<BrandName>Alienware</BrandName>" +
+                                    "<Features>" +
+                                       "<Feature>8th Generation Intel® Core™ i5 - 8300H</Feature>" +
+                                       "<Feature>Windows 10 Home 64 - bit English</Feature>" +
+                                       "<Feature>NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6</Feature>" +
+                                       "<Feature>8GB, 2x4GB, DDR4, 2666MHz</Feature>" +
+                                       "<Feature>1 TB of SSD</Feature>" +
+                                     "</Features>" +
+                                  "<Id>" + id + "</Id>" +
+                                  "<LaptopName>Alienware M17</LaptopName>" +
+                               "</Laptop>";
+
+            restResponse = HttpClientHelper.PerformPutRequest(putUrl, xmlData, xmlMediaType, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+
+            restResponse = HttpClientHelper.PerformGetRequest(getUrl + id, headers);
+            Assert.Equal(200, restResponse.StatusCode);
+  
+            Laptop xmlObj = ResponseDataHelper.DeserializeXmlResponse<Laptop>(restResponse.responseContent);
+            Assert.Contains("1 TB of SSD", xmlObj.Features.Feature);
+        }
     }
 }

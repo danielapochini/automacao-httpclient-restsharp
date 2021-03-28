f0433ae27b5c72277f74a5733f465c3ea45f663b
Daniela Pereira Pochini (TECH - Terceiro) <daniela.pochini@bs2tecnologia.com.br>
2021-03-27 21:35:23 -0300

    Método DELETE com autenticação

--- DeleteEndPoint/TestDeleteEndPoint.cs
+++ DeleteEndPoint/TestDeleteEndPoint.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WebServiceAutomation.Helpers.Authentication;
 using WebServiceAutomation.Helpers.Request;
 using WebServiceAutomation.Helpers.Response;
 using WebServiceAutomation.Model;
@@ -15,7 +16,9 @@ namespace WebServiceAutomation.DeleteEndPoint
     public class TestDeleteEndPoint
     {
         private string postUrl = "http://localhost:8080/laptop-bag/webapi/api/add/"; 
-        private string deleteUrl = "http://localhost:8080/laptop-bag/webapi/api/delete/";  
+        private string deleteUrl = "http://localhost:8080/laptop-bag/webapi/api/delete/";
+        private string securePostUrl = "http://localhost:8080/laptop-bag/webapi/secure/add/";
+        private string secureDeleteUrl = "http://localhost:8080/laptop-bag/webapi/secure/delete/";
         private Random random = new Random();
 
         [Fact]
@@ -49,6 +52,29 @@ namespace WebServiceAutomation.DeleteEndPoint
             Assert.Equal(200, (int)restResponse.StatusCode);
 
             restResponse = HttpClientHelper.PerformDeleteRequest(deleteUrl + id);
+
+            Assert.Equal(404, (int)restResponse.StatusCode);
+        }
+
+        [Fact]
+        public void TestSecureDeleteEndPoint()
+        {
+            int id = random.Next(1000);
+            RequestHelper.AddRecord(postUrl, id);
+
+            string authHeader = Base64StringConverter.GetBase64String("admin", "welcome");
+            authHeader = "Basic " + authHeader;
+
+            Dictionary<string, string> headers = new Dictionary<string, string>
+            {
+                {"Authorization", authHeader }
+            };
+
+            var restResponse = HttpClientHelper.PerformDeleteRequest(deleteUrl + id, headers);
+
+            Assert.Equal(200, (int)restResponse.StatusCode);
+
+            restResponse = HttpClientHelper.PerformDeleteRequest(deleteUrl + id, headers);
 
             Assert.Equal(404, (int)restResponse.StatusCode);
         }

66179fc874573fb0ba2a2615d48c8e9f821b2b46
Daniela Pereira Pochini (TECH - Terceiro) <daniela.pochini@bs2tecnologia.com.br>
2021-03-30 10:29:56 -0300

    Criando GET Request

---
+++ RestSharpAutomation/RestGetEndPoint/TestGetEndPoint.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RestSharpAutomation.RestGetEndPoint
+{
+    public class TestGetEndPoint
+    {
+        private readonly ITestOutputHelper output;
+
+        public TestGetEndPoint(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+         
+        private string getUrl = "http://localhost:8080/laptop-bag/webapi/api/all";
+         
+        [Fact]
+        public void TestGetUsingRestSharp()
+        {
+            IRestClient restClient = new RestClient();
+            IRestRequest restRequest = new RestRequest(getUrl);
+            IRestResponse restResponse = restClient.Get(restRequest);
+            output.WriteLine($"{restResponse.IsSuccessful}");
+            output.WriteLine($"{restResponse.StatusCode}");
+            output.WriteLine($"{restResponse.ErrorMessage}");
+            output.WriteLine($"{restResponse.ErrorException}");
+        }
+    }
+}

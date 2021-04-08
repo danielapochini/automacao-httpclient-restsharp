f4887a0d914115fb1a7bd1c1db55f13f34c53fd2
Daniela Pereira Pochini (TECH - Terceiro) <daniela.pochini@bs2tecnologia.com.br>
2021-04-07 23:05:20 -0300

    Novas implementações

---
+++ RestSharpAutomation/QueryParameter/QueryParameter.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebServiceAutomation.Model.XmlModel;
+using Xunit;
+
+namespace RestSharpAutomation.QueryParameter
+{
+    public class QueryParameter
+    {
+        private string searchUrl = "http://localhost:8080/laptop-bag/webapi/api/query";
+
+        [Fact]
+        public void TestQueryParameter()
+        {
+            IRestClient client = new RestClient();
+            IRestRequest request = new RestRequest()
+            {
+                Resource = searchUrl
+            };
+
+            request.AddHeader("Accept", "application/xml");
+            // 1st apporach 
+            //request.AddParameter("id", "1", ParameterType.QueryString);
+            request.AddQueryParameter("id", "1");
+            request.AddQueryParameter("laptopName", "Alienware M17");
+
+            var response = client.Get<Laptop>(request);
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("Alienware", response.Data.BrandName);
+
+        }
+    }
+}
---
+++ RestSharpAutomation/RestPostEndPoint/FileUploadEndPoint.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace RestSharpAutomation.RestPostEndPoint
+{
+    public class FileUploadEndPoint
+    {
+        [Fact]
+        public void Test_Upload_Of_File()
+        {
+            IRestClient client = new RestClient();
+            IRestRequest request = new RestRequest()
+            {
+                Resource = "https://jobportalkarate.herokuapp.com/normal/webapi/upload"
+            };
+
+            request.AddFile("file", @"C:\Cursos\WebServiceAutomation\RestSharpAutomation\tst.txt", "multipart/form-data");
+            var response = client.Post(request);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+    }
+}
